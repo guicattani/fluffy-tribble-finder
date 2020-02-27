00c13366e6bd31c2af893849bea8234f182b0b0f
Guilherme Cattani <gcattani1@gmail.com>
2020-02-27 16:04:33 -0300

    Repos

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
-    user: [],
+    repos: [],
+    user: {},
     loading: false,
     alert: null,
   };
@@ -37,6 +38,15 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
+    );
+
+    this.setState({ repos: res.data, loading: false });
+  };
+
   setAlert = (msg, type) => {
     this.setState({ alert: { msg, type } });
 
@@ -44,7 +54,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
 
     return (
       <Router>
@@ -75,7 +85,14 @@ class App extends Component {
                 exact
                 path="/user/:login"
                 render={props => (
-                  <User {...props} getUser={this.getUser} user={user} loading={loading} />
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
+                    repos={repos}
+                    user={user}
+                    loading={loading}
+                  />
                 )}
               />
             </Switch>
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,17 +1,21 @@
 import React, { Component, Fragment } from 'react';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
+import Repos from '../repos/Repos';
 import { Link } from 'react-router-dom';
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
 
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
     getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
+    repos: PropTypes.array.isRequired,
   };
 
   render() {
@@ -31,7 +35,7 @@ export class User extends Component {
       hireable,
     } = this.props.user;
 
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
     if (loading) {
       return <Spinner />;
@@ -39,7 +43,7 @@ export class User extends Component {
 
     return (
       <Fragment>
-        <Link to="/" classname="btn btn-light">
+        <Link to="/" className="btn btn-light">
           Back to Search
         </Link>
         Hireable:{' '}
@@ -55,15 +59,18 @@ export class User extends Component {
             <p>Location: {location}</p>
           </div>
 
-          {bio && (
-            <Fragment>
-              <h3>Bio</h3>
-              {bio}
-            </Fragment>
-          )}
-          <a href={html_url} className="btn btn-dark my-1">
-            Visit Github Profile
-          </a>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                {bio}
+              </Fragment>
+            )}
+            <br />
+            <a href={html_url} className="btn btn-dark my-1">
+              Visit Github Profile
+            </a>
+          </div>
           <ul>
             <li>
               {login && (
@@ -94,6 +101,7 @@ export class User extends Component {
           <div className="badge badge-light">Public Repos: {public_repos}</div>
           <div className="badge badge-dark">Public Gists: {public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
